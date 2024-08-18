adebf1a180c89ed9e69eb85bafd9ca5ab22e8414
natr <yc5@tju.edu.cn>
2024-08-18 14:24:12 +0800

    lab2: completed all

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -13,6 +13,10 @@ public class IntListExercises {
         while (head.rest != null) {
             head.first += c;
             head = head.rest;
+
+            if (head.rest == null) {
+                head.first += c;
+            }
         }
     }
 
@@ -26,7 +30,9 @@ public class IntListExercises {
     public static void setToZeroIfMaxFEL(IntList L) {
         IntList p = L;
         while (p != null) {
-            if (firstDigitEqualsLastDigit(max(p))) {
+            int currentMax = max(p);
+            boolean firstEqualsLast = firstDigitEqualsLastDigit(currentMax);
+            if (firstEqualsLast) {
                 p.first = 0;
             }
             p = p.rest;
@@ -51,7 +57,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
@@ -77,6 +83,8 @@ public class IntListExercises {
             lst.first *= lst.first;
         }
 
-        return currElemIsPrime || squarePrimes(lst.rest);
+        boolean restElemIsPrime = squarePrimes(lst.rest);
+
+        return currElemIsPrime || restElemIsPrime;
     }
 }
